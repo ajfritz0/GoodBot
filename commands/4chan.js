37f5b8f159464ff5c4197ed720d08c7a6d29adda
ajfritz0 <ajfritz@protonmail.com>
2021-10-18 12:23:44 -0700

    Fixed indentations

--- commands/4chan.js
+++ commands/4chan.js
@@ -3,26 +3,26 @@ const axios = require('axios');
 
 const getRandomIndex = (struct) => {
 	return struct[Math.floor(Math.random() * struct.length)];
-  }
-  
-  const getRandomThreadText = async (board) => {
+};
+
+const getRandomThreadText = async (board) => {
 	return new Promise((resolve, reject) => {
-	  if (!board || typeof board !== 'string') {
-		reject(new Error('Bad board :('));
-	  }
-  
-	  axios.get(`https://a.4cdn.org/${board}/catalog.json`)
-		.then((response) => {
-		  const { data } = response;
-		  const page = getRandomIndex(data);
-		  const thread = getRandomIndex(page.threads);
-		  resolve(thread.com);
-		})
-		.catch((error) => {
-		  reject(new Error('Could not complete request'));
-		});
-	});  
-  }
+		if (!board || typeof board !== 'string') {
+			reject(new Error('Bad board :('));
+		}
+
+		axios.get(`https://a.4cdn.org/${board}/catalog.json`)
+			.then((response) => {
+				const { data } = response;
+				const page = getRandomIndex(data);
+				const thread = getRandomIndex(page.threads);
+				resolve(thread.com);
+			})
+			.catch((error) => {
+				reject(new Error('Could not complete request'));
+			});
+	});
+};
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('4chan')
