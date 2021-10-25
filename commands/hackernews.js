328ec7ee8d8e9ed299191ffe87a2b971a13608a2
ospreydad <batscratchstevens@gmail.com>
2021-10-24 22:43:39 -0700

    Adding hackernews command

---
+++ commands/hackernews.js
@@ -0,0 +1,41 @@
+const axios = require('axios');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const getPostUrl = (id) => {
+    return `https://news.ycombinator.com/item?id=${id}`;
+};
+
+const getTopPosts = async (numPosts) => {
+    let response = '';
+    let limit = parseInt(numPosts, 10) || 10;
+
+    const { data } = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json');
+    if (!data) {
+        throw new Error('Could not complete request to HN API');
+    }
+
+    if (limit > data.length) {
+        limit = data.length;
+    }
+
+    const topIds = data.slice(0, limit);
+    topIds.forEach((id) => {
+        response += `${getPostUrl(id)}\n`
+    });
+    return response;
+};
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('hackernews')
+		.setDescription('Top Hackers News Posts')
+		.addStringOption(option =>
+			option.setName('numPosts')
+				.setDescription('The number of posts to return'),
+		),
+	async execute(interaction) {
+		const numPosts = interaction.options.getString('numPosts');
+		const response = await getTopPosts(numPosts);
+		return await interaction.reply(response);
+	},
+}
