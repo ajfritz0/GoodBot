89258d72c1fa649183439330b6c494acbdf85b27
ajfritz0 <ajfritz@protonmail.com>
2021-11-04 16:12:16 -0700

    Fixed the export statement in Markov library. Added functionality to the markov module; now collects the last 100 messages in the first channel to call it then spits out a generated phrase from the pool.

--- commands/markov.js
+++ commands/markov.js
@@ -1,10 +1,31 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const MarkovChain = require('../lib/Markov');
 
+const chain = new MarkovChain();
+
+let isCollected = false;
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('markov')
 		.setDescription('Markov Chain Text Generator'),
 	async execute(interaction) {
-		await interaction.reply('Nothing here for now');
+		if (isCollected == false) {
+			await interaction.deferReply();
+			let reply = '<EMPTY>';
+			try {
+				const messageCollection = await interaction.channel.messages.fetch({ limit: 100 });
+				messageCollection
+					.filter(m => m.type === 'DEFAULT')
+					.forEach(m => chain.addPhrase(m.content));
+				reply = chain.getPhrase();
+			}
+			catch (e) {
+				console.log(e);
+				reply = 'Unable to collect messages';
+			}
+			isCollected = true;
+			return await interaction.editReply(reply);
+		}
+		return await interaction.reply(chain.getPhrase());
 	},
 };
