09fa7ba6feff2c168e0b2188864a68b1c2e710b0
ajfritz0 <ajfritz@protonmail.com>
2021-11-05 19:04:50 -0700

    Added a maximum word length to the Markov library. Rewrite of the markov module; now listens for and stores new messages.

--- commands/markov.js
+++ commands/markov.js
@@ -1,31 +1,39 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const MarkovChain = require('../lib/Markov');
+const { readFile, writeFile } = require('fs/promises');
+const path = require('path');
+const sch = require('node-schedule');
 
-const chain = new MarkovChain();
+const dbPath = path.resolve(process.cwd(), 'databases/markovdb.json');
+const activeChannels = [];
 
-let isCollected = false;
-module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('markov')
-		.setDescription('Markov Chain Text Generator'),
-	async execute(interaction) {
-		if (isCollected == false) {
-			await interaction.deferReply();
-			let reply = '<EMPTY>';
-			try {
-				const messageCollection = await interaction.channel.messages.fetch({ limit: 100 });
-				messageCollection
-					.filter(m => m.type === 'DEFAULT')
-					.forEach(m => chain.addPhrase(m.content));
-				reply = chain.getPhrase();
-			}
-			catch (e) {
-				console.log(e);
-				reply = 'Unable to collect messages';
-			}
-			isCollected = true;
-			return await interaction.editReply(reply);
-		}
-		return await interaction.reply(chain.getPhrase());
-	},
+module.exports = function(client) {
+	const chain = new MarkovChain();
+	readFile(dbPath, { encoding: 'utf8' })
+		.then(data => chain._bank = JSON.parse(data))
+		.catch(console.error);
+
+	client.on('messageCreate', message => {
+		if (!activeChannels.includes(message.channelId)) return;
+
+		chain.addPhrase(message.content);
+	});
+
+	sch.scheduleJob('0 */4 * * *', () => {
+		console.log('Beginning database write');
+		writeFile(dbPath, JSON.stringify(chain._bank))
+			.then(() => console.log('Database write complete'))
+			.catch(console.error);
+	});
+	return {
+		data: new SlashCommandBuilder()
+			.setName('markov')
+			.setDescription('Markov Chain Text Generator'),
+		async execute(interaction) {
+			const _id = interaction.channelId;
+			if (!activeChannels.includes(_id)) activeChannels.push(_id);
+
+			return interaction.reply(chain.getPhrase());
+		},
+	};
 };
