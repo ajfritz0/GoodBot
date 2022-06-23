782bc79225d7d262b2442741e176f0156687d3b5
ajfritz0 <ajfritz@protonmail.com>
2022-06-23 16:06:29 -0700

    music player work in progress...

--- commands/pause.js
+++ commands/pause.js
@@ -5,7 +5,11 @@ module.exports = {
 		.setName('pause')
 		.setDescription('Pause playing audio'),
 	async execute(interaction) {
-		interaction.client.mp.togglePause();
+		const mpCollection = interaction.client.MusicPlayerCollection;
+		const mp = mpCollection.get(interaction.guild.id);
+		if (mp === null || mp === undefined) return interaction.reply('You have not added music');
+
+		mp.togglePause();
 		interaction.reply('Paused playback');
 		setTimeout(() => interaction.deleteReply(), 10 * 1000);
 	},
