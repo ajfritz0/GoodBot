bc0058a000c5a43620f84ac23b2f618fb4adb477
Joe Fritz <ajfritz0@protonmail.com>
2024-02-18 21:54:35 -0700

    simplify responses, tweaks

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,39 +1,47 @@
 const { SlashCommandBuilder } = require('discord.js');
 
+const responses = [
+	'It is certain',
+	'It is decidedly so',
+	'Without a doubt',
+	'Yes definitely',
+	'You may rely on it',
+	'As I see it, yes',
+	'Most likely',
+	'Outlook good',
+	'Yes',
+	'Signs point to yes',
+	'Reply hazy, try again',
+	'Ask again later',
+	'Better not tell you now',
+	'Cannot predict now',
+	'Concentrate and ask again',
+	'Don\'t count on it',
+	'My reply is no',
+	'My sources say no',
+	'Outlook not so good',
+	'Very doubtful',
+	'Maybe someday',
+	'Nothing',
+	'You cannot get to the top by sitting on your bottom',
+	'I see a new sauce in your future',
+	'Follow the seahorse',
+];
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('8ball')
-		.setDescription('Query the Magic 8ball'),
+		.setDescription('Query the Magic 8ball')
+		.addStringOption(option =>
+			option
+				.setName('question')
+				.setDescription('Ask your question'),
+		),
 	helpMessage: '',
-	async execute() {
-		const responses = [
-			'It is certain',
-			'It is decidedly so',
-			'Without a doubt',
-			'Yes definitely',
-			'You may rely on it',
-			'As I see it, yes',
-			'Most likely',
-			'Outlook good',
-			'Yes',
-			'Signs point to yes',
-			'Reply hazy, try again',
-			'Ask again later',
-			'Better not tell you now',
-			'Cannot predict now',
-			'Concentrate and ask again',
-			'Don\'t count on it',
-			'My reply is no',
-			'My sources say no',
-			'Outlook not so good',
-			'Very doubtful',
-			'Maybe someday',
-			'Nothing',
-			'You cannot get to the top by sitting on your bottom',
-			'I see a new sauce in your future',
-			'Follow the seahorse',
-		];
-
-		return responses[Math.floor(Math.random() * responses.length)];
+	async execute(interaction) {
+		const q = interaction.options.getString('question');
+		const r = responses[Math.floor(Math.random() * responses.length)];
+		if (q == null || q == undefined) return `**${r}**`;
+		else return `Q: ${q}\n**${r}**`;
 	},
 };
--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,113 +1,31 @@
 const { Events } = require('discord.js');
-const { readFile, readdir } = require('fs/promises');
-const { resolve } = require('path');
 
 let bReady = false;
+let phrases;
 
-const phrases = {
-	generic: {},
-	userCustom: {},
-};
+try {
+	phrases = require('./databases/responses');
+	if (phrases.length == 0) throw new Error('NO RESPONSES');
+	bReady = true;
+}
+catch (e) {
+	console.error(e);
+}
 
 function randint(min, max) {
 	return Math.floor(Math.random() * (max - min)) + min;
 }
 
-async function importData(filename) {
-	try {
-		const rawData = await readFile(filename, 'utf8');
-		if (rawData.length == 0) return { error: 1, message: 'File contents empty' };
-
-		const data = rawData.split('\n').filter(value => value.length > 0);
-		return {
-			error: null,
-			data,
-		};
-	}
-	catch (e) {
-		return {
-			error: 2,
-			message: e,
-		};
-	}
-}
-
-(async () => {
-	try {
-		console.log('=====');
-		const genericResponsePath = resolve('databases', 'generic_responses');
-		const customUserReponsePath = resolve('databases', 'user_responses');
-		const genericDirents = await readdir(genericResponsePath);
-		const userDirents = await readdir(customUserReponsePath);
-		for (const file of genericDirents) {
-			const fileContents = await importData(resolve(genericResponsePath, file));
-			if (fileContents.error) {
-				console.error(fileContents.message);
-			}
-			else {
-				phrases.generic[file] = fileContents.data;
-			}
-		}
-		for (const file of userDirents) {
-			const fileContents = await importData(resolve(customUserReponsePath, file));
-			if (fileContents.error) {
-				console.error(fileContents.message);
-			}
-			else {
-				phrases.userCustom[file] = fileContents.data;
-			}
-		}
-		console.log('=====');
-		bReady = true;
-	}
-	catch (e) {
-		console.log('message create not available');
-		console.log(e);
-	}
-})();
-
 module.exports = {
 	type: Events.MessageCreate,
 	once: false,
 	execute(message) {
-		if (message.author.bot || bReady == false || Object.keys(phrases.generic).length == 0) return;
-		const messageAuthorName = message.author.username;
+		if (message.author.bot || bReady == false) return;
 		const botID = message.client.user.id;
-		const chanName = message.channel.name;
 		const userMentions = message.mentions.users;
-		const guildName = message.guild.name;
 
 		if (userMentions.has(botID)) {
-			// check if the user has any custom responses
-			// Failing that check, send a generic response to the author
-
-			console.log(`Message Author: ${messageAuthorName} / Guild: ${guildName} Channel: ${chanName}`);
-			let easteregg = 0;
-			const usersWithReponses = Object.keys(phrases.userCustom);
-
-			if (usersWithReponses.includes(messageAuthorName)) easteregg = randint(0, 100);
-
-			if (easteregg == 99) {
-				const userMessages = phrases.userCustom[messageAuthorName];
-				message.channel.send(userMessages[randint(0, userMessages.length)]);
-			}
-			else {
-				const genericKeys = Object.keys(phrases.generic);
-				for (const key of genericKeys) {
-					if (message.content.includes(key)) {
-						const arr = phrases.generic[key];
-						message.channel.send(
-							arr[randint(0, arr.length)],
-						);
-						return;
-					}
-				}
-				const key = genericKeys[randint(0, genericKeys.length)];
-				const phraseArr = phrases.generic[key];
-				message.channel.send(
-					phraseArr[randint(0, phraseArr.length)],
-				);
-			}
+			message.channel.send(phrases[randint(0, phrases.length)]);
 		}
 	},
 };
